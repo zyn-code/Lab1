5d74e8b6cdbce04020ac970fa6b6a6a1f3bbc432
zyn-code <98745109+zyn-code@users.noreply.github.com>
2022-06-27 10:17:21 +0300

    first commit

---
+++ Controllers/Student.cs
@@ -0,0 +1,11 @@
+namespace WebApplication1.Controllers;
+
+public class Student
+{
+    private long id;
+    private String name;
+    private String email;
+    
+    List<Student> students = new List<Student>();    
+
+}
